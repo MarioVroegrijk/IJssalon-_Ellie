e1a721fc5ca444497a228e3b48d5cbad79b3bdbf
Mario <mvroegrijk@gmail.com>
2025-11-12 20:08:05 +0100

    bestand tijdelijk.py gecodeerd

--- Tijdelijk.py
+++ Tijdelijk.py
@@ -6,10 +6,29 @@ prijzen = {
 
 aanbieding = prijzen["aardbei"] * 0.8
 
-# reclame_tekst met puntnotatie
+# reclame_tekst met 2 decimalen
 reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € {aanbieding:.2f}"
 
-# reclame_tekst2 met komma i.p.v. punt (Nederlandse notatie)
-reclame_tekst2 = reclame_tekst.replace('.', ',')
+# vind de eerste punt in de prijs
+comma_pos = reclame_tekst.find(".")
+# zoek de eerste '0' na de punt
+if comma_pos != -1:
+    index_of_zero = reclame_tekst.find("0", comma_pos + 1)
+else:
+    index_of_zero = -1
 
-print(reclame_tekst2)
+# reclame_tekst2 met index
+reclame_tekst2 = f"{reclame_tekst}[: {index_of_zero}]"
+
+# reclame_tekst3 in hoofdletters
+reclame_tekst3 = reclame_tekst2.upper()
+
+# reclame_tekst4 als lijst van woorden
+reclame_tekst4 = reclame_tekst3.split()
+
+# loop door de woorden en pas hoofd-/kleine letters toe afhankelijk van lengte
+for el in reclame_tekst4:
+    if len(el) >= 5:
+        print(el.upper())
+    else:
+        print(el.lower())
